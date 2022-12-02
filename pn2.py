1c83277e149484b7fb387d84d24e93d646d23ffa
Evergardener <henry0927@outlook.com>
2022-12-02 19:28:01 +0800

    Main source code

--- pn2.py
+++ pn2.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os 
 import cv2 as cv
-from keras.models import Model, load_model
+from keras.models import Model
 from keras.optimizers import Adam
 from keras import backend
+from keras.utils import plot_model
 from keras.layers import Input, Add, Activation, BatchNormalization, Conv2D,\
      DepthwiseConv2D, UpSampling2D, MaxPooling2D, Dropout
 
@@ -25,22 +26,22 @@ def D_Block(input_layer, filts, isConcat=False, concat_layer=None):
     else:
         bl = input_layer
     # Left conv 1x1
-    bl_left_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last')(bl)
+    bl_left_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last', use_bias=False)(bl)
     bl_left_route = BatchNormalization()(bl_left_route)
     # Right conv dw 3x3
-    bl_right_route = DepthwiseConv2D(kernel_size=3, strides=1, padding='same', data_format='channels_last')(bl)
+    bl_right_route = DepthwiseConv2D(kernel_size=3, strides=1, padding='same', data_format='channels_last', use_bias=False)(bl)
     bl_right_route = BatchNormalization()(bl_right_route)
     bl_right_route = Activation('relu')(bl_right_route)
     # Right conv 1x1
-    bl_right_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last')(bl_right_route)
+    bl_right_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last', use_bias=False)(bl_right_route)
     bl_right_route = BatchNormalization()(bl_right_route)
     bl_right_route = Activation('relu')(bl_right_route)
     # Right conv dw 3x3
-    bl_right_route = DepthwiseConv2D(kernel_size=3, strides=1, padding='same', data_format='channels_last')(bl_right_route)
+    bl_right_route = DepthwiseConv2D(kernel_size=3, strides=1, padding='same', data_format='channels_last', use_bias=False)(bl_right_route)
     bl_right_route = BatchNormalization()(bl_right_route)
     bl_right_route = Activation('relu')(bl_right_route)
     # Right conv 1x1
-    bl_right_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last')(bl_right_route)
+    bl_right_route = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last', use_bias=False)(bl_right_route)
     bl_right_route = BatchNormalization()(bl_right_route)
     # Final concatenation
     bl_concat = Add()([bl_left_route, bl_right_route])
@@ -54,7 +55,7 @@ def D_stage(input_layer, filts, isConcat=False, concat_layer=None):
     return bl
 
 def E_stage(input_layer, filts):
-    bl = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last')(input_layer)
+    bl = Conv2D(filters=filts, kernel_size=1, strides=1, padding='same', data_format='channels_last', use_bias=False)(input_layer)
     bl = BatchNormalization()(bl)
     bl = Activation('relu')(bl)
     bl = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(bl)
@@ -62,8 +63,8 @@ def E_stage(input_layer, filts):
 
 # Data preprocess
 print('\nRunning Data Preprocess...')
-X_route = 'D:/Assign_3/EG1800/input/'
-Y_route = 'D:/Assign_3/EG1800/mask/'
+X_route = 'EG1800/input/'
+Y_route = 'EG1800/mask/'
 files = os.listdir(X_route)
 dataCount = 0
 
@@ -71,7 +72,7 @@ dataCount = 0
 for file in files:
     dataCount += 1
 
-dataSize = 128
+dataSize = 224
 X_chs = 3
 X_data = np.empty((dataCount, dataSize, dataSize, X_chs), dtype=np.float16)
 Y_mask = np.empty((dataCount, dataSize, dataSize), dtype=np.float16)
@@ -87,13 +88,11 @@ for i in range(0, 2632):
     maskMat = cv.imread(mask)
     imgMat = cv.resize(imgMat, (dataSize, dataSize))
     maskMat = cv.resize(maskMat, (dataSize, dataSize))
-    X_data[count] = imgMat
+    X_data[count] = imgMat / 255
     Y_mask[count] = cv.cvtColor(maskMat, cv.COLOR_BGR2GRAY) / 255
     maskMat_bin = cv.cvtColor(maskMat, cv.COLOR_BGR2GRAY)
-    canny = cv.Canny(maskMat_bin, 50, 255)
-    Y_boundary[count] = canny / 255
+    Y_boundary[count] = cv.Canny(maskMat_bin, 50, 255) / 255
     count += 1
-    print('Complete for data index ', count)
 
 Y_mask = tf.expand_dims(Y_mask, axis=-1)
 Y_boundary = tf.expand_dims(Y_boundary, axis=-1)
@@ -104,35 +103,38 @@ print('Preprocess complete.\nTraining start...')
 # Data preprocess complete
 
 main_input = Input((dataSize, dataSize, X_chs), name='main_input')
-initlyr = Conv2D(filters=1, kernel_size=1, padding='same', data_format='channels_last')(main_input)
+initlyr = Conv2D(filters=1, kernel_size=1, padding='same', data_format='channels_last', use_bias=False)(main_input)
 
-enclyr_1 = E_stage(initlyr, 16) # -> 64x64x16
-enclyr_2 = E_stage(enclyr_1, 32) # -> 32x32x32
-enclyr_3 = E_stage(enclyr_2, 64) # -> 16x16x64
-enclyr_4 = E_stage(enclyr_3, 128) # -> 8x8x128
-enclyr_5 = E_stage(enclyr_4, 256) # -> 4x4x256
+enclyr_1 = E_stage(initlyr, 32)
+enclyr_2 = E_stage(enclyr_1, 64)
+enclyr_3 = E_stage(enclyr_2, 128) 
+enclyr_4 = E_stage(enclyr_3, 256)
+enclyr_5 = E_stage(enclyr_4, 512)
 
-declyr_1 = D_stage(enclyr_5, 128) # -> 8x8x128
-declyr_2 = D_stage(declyr_1, 64, True, enclyr_4) # -> 16x16x64
-declyr_3 = D_stage(declyr_2, 32, True, enclyr_3) # -> 32x32x32
-declyr_4 = D_stage(declyr_3, 16, True, enclyr_2) # -> 64x64x16
-declyr_5 = D_stage(declyr_4, 8, True, enclyr_1) # -> 128x128x8
+declyr_1 = D_stage(enclyr_5, 256)
+declyr_2 = D_stage(declyr_1, 128, True, enclyr_4)
+declyr_3 = D_stage(declyr_2, 64, True, enclyr_3)
+declyr_4 = D_stage(declyr_3, 32, True, enclyr_2)
+declyr_5 = D_stage(declyr_4, 16, True, enclyr_1)
 
 mask = Conv2D(filters=1, kernel_size=1, use_bias=False, data_format='channels_last', activation='sigmoid')(declyr_5)
+print(mask)
 boundary = Conv2D(filters=1, kernel_size=1, use_bias=False, data_format='channels_last', activation='sigmoid')(declyr_5)
+print(boundary)
 
 model = Model(main_input, [mask, boundary], name='PortraitNet')
+plot_model(model, to_file='structure.png')
 
 adam = Adam(learning_rate=1e-3)
 
 model.compile(
     optimizer=adam,
     loss=['binary_crossentropy', binary_focal_loss],
-    loss_weights=[1., 1.],
+    loss_weights=[1., 0.1],
     metrics=[['accuracy'], ['accuracy']]    
 )
 
-history = model.fit(X_train, [Y_mask_train, Y_boundary_train], batch_size=32, epochs=50)
+history = model.fit(X_train, [Y_mask_train, Y_boundary_train], batch_size=16, epochs=25)
 print(history.history.keys())
 
 loss = model.evaluate(X_test, [Y_mask_test, Y_boundary_test])
@@ -145,17 +147,13 @@ plt.plot(history.history['conv2d_22_accuracy'])
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['Mask', 'Boundary'], loc='lower right')
-plt.show()
+plt.savefig('accu.png')
 
 plt.plot(history.history['conv2d_21_loss'])
 plt.plot(history.history['conv2d_22_loss'])
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['Mask', 'Boundary'], loc='upper right')
-plt.show()
+plt.savefig('loss.png')
 
-# Last Result: loss: 0.3654 - conv2d_21_loss: 0.3553 - conv2d_22_loss: 0.0101 - conv2d_21_accuracy: 0.8972 - conv2d_22_accuracy: 0.9817
-
-
-
-
+# - loss: 0.2689 - conv2d_21_loss: 0.2682 - conv2d_22_loss: 0.0065 - conv2d_21_accuracy: 0.9140 - conv2d_22_accuracy: 0.9884
